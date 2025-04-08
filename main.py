4553e46d0c6255dac34939274bbd3cb1810afff2
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-08 14:53:42 +0300

    class+db

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 import mysql.connector
 
-
-class Sosiski:
-    def __init__(self, name, price, date):
-        self.name = name
-        self.price = price
-        self.date = date
-
-
-cnx = mysql.connector.connect(
-    host="localhost",
-    user="pk21",
-    password="1234",
-    database="sosiski")
-cursor = cnx.cursor()
-cursor.execute("SELECT * FROM sosiski")
-ans = cursor.fetchall()
-print(ans)
-sosiski = []
+con = mysql.connector.connect(host="localhost",
+                        user='pk21',
+                        password="1234",
+                        database='items')
+cur = con.cursor()
+cur.execute("SELECT * FROM items")
+ans = cur.fetchall()
 for item in ans:
-    tmp_sosiska = Sosiski(item[1],item[2],item[3])
-    sosiski.append(tmp_sosiska)
-
-for item in sosiski:
-    print(item.name, item.price, item.date)
+    print(item)

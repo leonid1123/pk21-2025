ab4d268f7ebec3003d1f416c1cf95cbc130a8bc4
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-15 12:00:32 +0300

    class+db+tk

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from tkinter import *
 
 class App:
     def __init__(self):
+        my_list = []
+        font_style = {'font': ('Helvetica', 16)}
         con = mysql.connector.connect(host="localhost",
                         user='pk21',
                         password="1234",
@@ -11,13 +13,13 @@ class App:
         cur.execute("SELECT * FROM items")
         ans = cur.fetchall()
         for item in ans:
-            print(item)
+            tmp = f"Название:{item[3]}, тип:{item[1]}, цена:{item[2]} золотых"
+            my_list.append(tmp)
         win = Tk()
         win.title("Инвентарь и вещи")
-        win.geometry("300x300")
-        my_list = ["пупупу","чпунь","хочу 5"]
+        win.geometry("800x300")
         my_list_var = Variable(value=my_list)
-        items_view = Listbox(listvariable=my_list_var)
+        items_view = Listbox(listvariable=my_list_var, width=80, **font_style)
         items_view.grid(row=0, column=0)
         win.mainloop()
 

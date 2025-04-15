e6a8c628ee613855670defc205976a98ec75f777
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-15 11:13:06 +0300

    class+db+tk

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import mysql.connector
+from tkinter import *
 
-con = mysql.connector.connect(host="localhost",
+class App:
+    def __init__(self):
+        con = mysql.connector.connect(host="localhost",
                         user='pk21',
                         password="1234",
                         database='items')
-cur = con.cursor()
-cur.execute("SELECT * FROM items")
-ans = cur.fetchall()
-for item in ans:
-    print(item)
+        cur = con.cursor()
+        cur.execute("SELECT * FROM items")
+        ans = cur.fetchall()
+        for item in ans:
+            print(item)
+        win = Tk()
+        win.title("Инвентарь и вещи")
+        win.geometry("300x300")
+        my_list = ["пупупу","чпунь","хочу 5"]
+        my_list_var = Variable(value=my_list)
+        items_view = Listbox(listvariable=my_list_var)
+        items_view.grid(row=0, column=0)
+        win.mainloop()
+
+
+app = App()

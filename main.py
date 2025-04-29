6a628b3a926e441279b00bfffe90a7b600f4c242
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2025-04-29 10:19:05 +0300

    готово

--- main.py
+++ main.py
@@ -6,29 +6,43 @@ class App:
     def __init__(self):
         self.my_list = []
         self.my_inv = ["что-то"]
-        font_style = {'font': ('Helvetica', 16)}
+        self.font_style = {'font': ('Helvetica', 16)}
         self.db_connect()
         self.get_all_items()
         self.get_all_inv()
 
-        win = Tk()
+        self.win = Tk()
         self.setUI()
 
-        win.mainloop()
+        self.win.mainloop()
 
     def setUI(self):
-        win.title("Инвентарь и вещи")
-        win.geometry("1000x600")
+        self.win.title("Инвентарь и вещи")
+        self.win.geometry("1300x600")
         my_list_var = Variable(value=self.my_list)
-        items_view = Listbox(listvariable=my_list_var, width=80, **font_style)
-        items_view.grid(row=1, column=0)
-        lbl_items = Label(text="Вещи", **font_style)
+        items_view = Listbox(listvariable=my_list_var, width=80, **self.font_style)
+        items_view.grid(row=1, column=0, rowspan=4)
+        lbl_items = Label(text="Вещи", **self.font_style)
         lbl_items.grid(row=0, column=0)
-        lbl_inv = Label(text="Инвентарь", **font_style)
-        lbl_inv.grid(row=2, column=0)
+        lbl_inv = Label(text="Инвентарь", **self.font_style)
+        lbl_inv.grid(row=5, column=0)
         my_inv_var = Variable(value=self.my_inv)
-        inv_view = Listbox(listvariable=my_inv_var, width=80, **font_style)
-        inv_view.grid(row=3, column=0)
+        inv_view = Listbox(listvariable=my_inv_var, width=80, **self.font_style)
+        inv_view.grid(row=6, column=0)
+        name_lbl = Label(text="Название")
+        type_lbl = Label(text="Тип")
+        price_lbl = Label(text="Цена")
+        name_lbl.grid(row=1, column=1)
+        type_lbl.grid(row=2, column=1)
+        price_lbl.grid(row=3, column=1)
+        name_entry = Entry()
+        type_entry = Entry()
+        price_entry = Entry()
+        name_entry.grid(row=1, column=2)
+        type_entry.grid(row=2, column=2)
+        price_entry.grid(row=3, column=2)
+        add_item_btn = Button(text="Добавить")
+        add_item_btn.grid(row=4, column=1, columnspan=2)
 
     def db_connect(self):
         """метод для подключения к базе данных.
@@ -45,7 +59,7 @@ class App:
         self.cur.execute("SELECT * FROM items")
         ans = self.cur.fetchall()
         for item in ans:
-            tmp = f"Название:{item[3]}, тип:{item[1]}, цена:{item[2]} золотых"
+            tmp = f"Название:{item[1]}, тип:{item[2]}, цена:{item[3]} золотых"
             self.my_list.append(tmp)
 
     def get_all_inv(self):
@@ -53,7 +67,7 @@ class App:
         sql = """SELECT items.name, inventory.quantity
         FROM inventory
         INNER JOIN items
-        ON inventory.item_id = items.id_items"""
+        ON inventory.id_item = items.id"""
         self.cur.execute(sql)
         ans = self.cur.fetchall()
         for item in ans:
